e83220c386a2f1a4b521e8a718678194c87f2f58
pscott <scott.piriou@ledger.fr>
2021-10-01 08:01:50 +0200

    Add EDIT THIS in js tests

--- tests/src/swap_exact_eth_for_tokens.test.js
+++ tests/src/swap_exact_eth_for_tokens.test.js
@@ -7,7 +7,8 @@ import { parseEther, parseUnits} from "ethers/lib/utils";
 // EDIT THIS: Replace with your contract address
 const contractAddr = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d";
 // EDIT THIS: Replace `boilerplate` with your plugin name
-const abi_path = '../boilerplate/abis/' + contractAddr + '.json';
+const pluginName = "boilerplate";
+const abi_path = `../${pluginname}/abis/` + contractAddr + '.json';
 const abi = require(abi_path);
 
 
@@ -15,17 +16,22 @@ const abi = require(abi_path);
 // https://etherscan.io/tx/0x0160b3aec12fd08e6be0040616c7c38248efb4413168a3372fc4d2db0e5961bb
 
 // Nanos S test
+// EDIT THIS: build your own test
 test('[Nano S] Swap Exact Eth For Tokens with beneficiary', zemu("nanos", async (sim, eth) => {
+  const contract = new ethers.Contract(contractAddr, abi);
+
   // Constants used to create the transaction
+  // EDIT THIS: Remove what you don't need
   const amountOutMin = parseUnits("28471151959593036279", 'wei');
   const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
   const SUSHI = "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2";
   const path = [WETH, SUSHI];
   const deadline = Number(1632843280);
   const beneficiary = "0x15557c8b7246C38EE71eA6dc69e4347F5DAc2104";
-  const contract = new ethers.Contract(contractAddr, abi);
 
+  // EDIT THIS: adapt the signature to your method
   // signature: swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+  // EDIT THIS: don't call `swapExactETHForTokens` but your own method and adapt the arguments.
   const {data} = await contract.populateTransaction.swapExactETHForTokens(amountOutMin, path, beneficiary, deadline);
 
   // Get the generic transaction template
@@ -34,6 +40,7 @@ test('[Nano S] Swap Exact Eth For Tokens with beneficiary', zemu("nanos", async 
   unsignedTx.to = contractAddr;
   // Modify the attached data
   unsignedTx.data = data;
+  // EDIT THIS: get rid of this if you don't wish to modify the `value` field.
   // Modify the number of ETH sent
   unsignedTx.value = parseEther("0.1");
 
@@ -53,12 +60,14 @@ test('[Nano S] Swap Exact Eth For Tokens with beneficiary', zemu("nanos", async 
   // Wait for the application to actually load and parse the transaction
   await waitForAppScreen(sim);
   // Navigate the display by pressing the right button 10 times, then pressing both buttons to accept the transaction.
+  // EDIT THIS: modify `10` to fix the number of screens you are expecting to navigate through.
   await sim.navigateAndCompareSnapshots('.', 'nanos_swap_exact_eth_for_tokens_with_beneficiary', [10, 0]);
 
   await tx;
 }));
 
 // NanoX test
+// EDIT THIS: build your own test
 test('[Nano X] Swap Exact Eth For Tokens with beneficiary', zemu("nanox", async (sim, eth) => {
 
   const serializedTx = txFromEtherscan("0x02f9015a0181d38459682f0085215d7c1e598302a4e9947a250d5630b4cf539739df2c5dacb4c659f2488d88016345785d8a0000b8e47ff36ab50000000000000000000000000000000000000000000000018b1dd9dc51b5a9f7000000000000000000000000000000000000000000000000000000000000008000000000000000000000000015557c8b7246c38ee71ea6dc69e4347f5dac210400000000000000000000000000000000000000000000000000000000615336100000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006b3595068778dd592e39a122f4f5a5cf09c90fe2c001a089c5ce4ce199f7d93ea1d54c08133fab9407d8de63a9885d59d8ce69a59573dda045f6a0e0d3288dfdfddc23ad0afb9577c4011801f598d581a46cd0b0e2bd0571");
