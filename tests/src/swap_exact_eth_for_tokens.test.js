0096f18806cd95bb5afcff00ea0093173ee114ab
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2022-02-10 18:20:35 +0100

    Refactor tests

--- tests/src/swap_exact_eth_for_tokens.test.js
+++ tests/src/swap_exact_eth_for_tokens.test.js
@@ -1,6 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import { waitForAppScreen, zemu, genericTx, SPECULOS_ADDRESS, RANDOM_ADDRESS, txFromEtherscan} from './test.fixture';
+import { waitForAppScreen, zemu, genericTx, nano_models,SPECULOS_ADDRESS, txFromEtherscan} from './test.fixture';
 import { ethers } from "ethers";
 import { parseEther, parseUnits} from "ethers/lib/utils";
 
@@ -11,13 +11,34 @@ const pluginName = "boilerplate";
 const abi_path = `../${pluginName}/abis/` + contractAddr + '.json';
 const abi = require(abi_path);
 
-
-// Reference transaction for this test:
-// https://etherscan.io/tx/0x0160b3aec12fd08e6be0040616c7c38248efb4413168a3372fc4d2db0e5961bb
-
-// Nanos S test
+// Test from replayed transaction: https://etherscan.io/tx/0x0160b3aec12fd08e6be0040616c7c38248efb4413168a3372fc4d2db0e5961bb
 // EDIT THIS: build your own test
-test('[Nano S] Swap Exact Eth For Tokens with beneficiary', zemu("nanos", async (sim, eth) => {
+nano_models.forEach(function(model) {
+  test('[Nano ' + model.letter + '] Swap Exact Eth For Tokens with beneficiary', zemu(model, async (sim, eth) => {
+
+  // The rawTx of the tx up above is accessible through: https://etherscan.io/getRawTx?tx=0xb27a69cd3190ad0712da39f6b809ecc019ecbc319d3c17169853270226d18a8a
+  const serializedTx = txFromEtherscan("0x02f9015a0181d38459682f0085215d7c1e598302a4e9947a250d5630b4cf539739df2c5dacb4c659f2488d88016345785d8a0000b8e47ff36ab50000000000000000000000000000000000000000000000018b1dd9dc51b5a9f7000000000000000000000000000000000000000000000000000000000000008000000000000000000000000015557c8b7246c38ee71ea6dc69e4347f5dac210400000000000000000000000000000000000000000000000000000000615336100000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006b3595068778dd592e39a122f4f5a5cf09c90fe2c001a089c5ce4ce199f7d93ea1d54c08133fab9407d8de63a9885d59d8ce69a59573dda045f6a0e0d3288dfdfddc23ad0afb9577c4011801f598d581a46cd0b0e2bd0571");
+
+  const tx = eth.signTransaction(
+    "44'/60'/0'/0",
+    serializedTx,
+  );
+
+  const right_clicks = model.letter === 'S' ? 12 : 6;
+
+  // Wait for the application to actually load and parse the transaction
+  await waitForAppScreen(sim);
+  // Navigate the display by pressing the right button `right_clicks` times, then pressing both buttons to accept the transaction.
+  await sim.navigateAndCompareSnapshots('.', model.name + '_swap_exact_eth_for_tokens_with_beneficiary', [right_clicks, 0]);
+
+  await tx;
+  }));
+});
+
+// Test from constructed transaction
+// EDIT THIS: build your own test
+nano_models.forEach(function(model) {
+  test('[Nano ' + model.letter + '] Swap Exact Eth For Tokens', zemu(model, async (sim, eth) => {
   const contract = new ethers.Contract(contractAddr, abi);
 
   // Constants used to create the transaction
@@ -27,12 +48,13 @@ test('[Nano S] Swap Exact Eth For Tokens with beneficiary', zemu("nanos", async 
   const SUSHI = "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2";
   const path = [WETH, SUSHI];
   const deadline = Number(1632843280);
-  const beneficiary = "0x15557c8b7246C38EE71eA6dc69e4347F5DAc2104";
+  // We set beneficiary to the default address of the emulator, so it maches sender address
+  const beneficiary = SPECULOS_ADDRESS;
 
   // EDIT THIS: adapt the signature to your method
   // signature: swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
   // EDIT THIS: don't call `swapExactETHForTokens` but your own method and adapt the arguments.
-  const {data} = await contract.populateTransaction.swapExactETHForTokens(amountOutMin, path, beneficiary, deadline);
+  const {data} = await contract.populateTransaction.swapExactETHForTokens(amountOutMin, path, beneficiary ,deadline);
 
   // Get the generic transaction template
   let unsignedTx = genericTx;
@@ -47,40 +69,20 @@ test('[Nano S] Swap Exact Eth For Tokens with beneficiary', zemu("nanos", async 
   // Create serializedTx and remove the "0x" prefix
   const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
 
-  // Rather than constructing the tx ourselves, one can can obtain it directly through etherscan.
-  // The rawTx of the tx up above is accessible through: https://etherscan.io/getRawTx?tx=0xb27a69cd3190ad0712da39f6b809ecc019ecbc319d3c17169853270226d18a8a
-  // In this case, you could remove the above code, and simply create your tx like so:
-  // const serializedTx = txFromEtherscan("0x02f9015a0181d38459682f0085215d7c1e598302a4e9947a250d5630b4cf539739df2c5dacb4c659f2488d88016345785d8a0000b8e47ff36ab50000000000000000000000000000000000000000000000018b1dd9dc51b5a9f7000000000000000000000000000000000000000000000000000000000000008000000000000000000000000015557c8b7246c38ee71ea6dc69e4347f5dac210400000000000000000000000000000000000000000000000000000000615336100000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006b3595068778dd592e39a122f4f5a5cf09c90fe2c001a089c5ce4ce199f7d93ea1d54c08133fab9407d8de63a9885d59d8ce69a59573dda045f6a0e0d3288dfdfddc23ad0afb9577c4011801f598d581a46cd0b0e2bd0571");
-
   const tx = eth.signTransaction(
     "44'/60'/0'/0",
     serializedTx
   );
 
+  const right_clicks = model.letter === 'S' ? 7 : 5;
+
   // Wait for the application to actually load and parse the transaction
   await waitForAppScreen(sim);
   // Navigate the display by pressing the right button 10 times, then pressing both buttons to accept the transaction.
   // EDIT THIS: modify `10` to fix the number of screens you are expecting to navigate through.
-  await sim.navigateAndCompareSnapshots('.', 'nanos_swap_exact_eth_for_tokens_with_beneficiary', [10, 0]);
+  await sim.navigateAndCompareSnapshots('.', model.name + '_swap_exact_eth_for_tokens', [right_clicks, 0]);
 
   await tx;
-}));
+  }));
+});
 
-// NanoX test
-// EDIT THIS: build your own test
-test.skip('[Nano X] Swap Exact Eth For Tokens with beneficiary', zemu("nanox", async (sim, eth) => {
-
-  const serializedTx = txFromEtherscan("0x02f9015a0181d38459682f0085215d7c1e598302a4e9947a250d5630b4cf539739df2c5dacb4c659f2488d88016345785d8a0000b8e47ff36ab50000000000000000000000000000000000000000000000018b1dd9dc51b5a9f7000000000000000000000000000000000000000000000000000000000000008000000000000000000000000015557c8b7246c38ee71ea6dc69e4347f5dac210400000000000000000000000000000000000000000000000000000000615336100000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006b3595068778dd592e39a122f4f5a5cf09c90fe2c001a089c5ce4ce199f7d93ea1d54c08133fab9407d8de63a9885d59d8ce69a59573dda045f6a0e0d3288dfdfddc23ad0afb9577c4011801f598d581a46cd0b0e2bd0571");
-
-  const tx = eth.signTransaction(
-    "44'/60'/0'/0",
-    serializedTx,
-  );
-
-  // Wait for the application to actually load and parse the transaction
-  await waitForAppScreen(sim);
-  // Navigate the display by pressing the right button 6 times, then pressing both buttons to accept the transaction.
-  await sim.navigateAndCompareSnapshots('.', 'nanox_swap_exact_eth_for_tokens_with_beneficiary', [6, 0]);
-
-  await tx;
-}));
--- tests/src/test.fixture.js
+++ tests/src/test.fixture.js
@@ -1,4 +1,4 @@
-import Zemu from '@zondax/zemu';
+import Zemu, { DEFAULT_START_OPTIONS, DeviceModel } from '@zondax/zemu';
 import Eth from '@ledgerhq/hw-app-eth';
 import { generate_plugin_config } from './generate_plugin_config';
 import { parseEther, parseUnits, RLP} from "ethers/lib/utils";
@@ -9,12 +9,12 @@ async function waitForAppScreen(sim) {
     await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot(), transactionUploadDelay);
 }
 
-const sim_options_generic = {
+const sim_options_nano = {
+    ...DEFAULT_START_OPTIONS,
     logging: true,
     X11: true,
     startDelay: 5000,
-    startText: 'is ready',
-    custom: '',
+    startText: 'is ready'
 };
 
 const Resolve = require('path').resolve;
@@ -25,9 +25,11 @@ const NANOX_ETH_PATH = Resolve('elfs/ethereum_nanox.elf');
 const NANOS_PLUGIN_PATH = Resolve('elfs/plugin_nanos.elf');
 const NANOX_PLUGIN_PATH = Resolve('elfs/plugin_nanox.elf');
 
-// Edit this: replace `Boilerplate` by your plugin name
-const NANOS_PLUGIN = { "Boilerplate": NANOS_PLUGIN_PATH };
-const NANOX_PLUGIN = { "Boilerplate": NANOX_PLUGIN_PATH };
+const nano_models: DeviceModel[] = [
+    { name: 'nanos', letter: 'S', path: NANOS_PLUGIN_PATH, eth_path: NANOS_ETH_PATH },
+    { name: 'nanox', letter: 'X', path: NANOX_PLUGIN_PATH, eth_path: NANOX_ETH_PATH }
+];
+
 
 const boilerplateJSON = generate_plugin_config();
 
@@ -79,24 +81,15 @@ function txFromEtherscan(rawTx) {
 function zemu(device, func) {
     return async () => {
         jest.setTimeout(TIMEOUT);
-        let eth_path;
-        let plugin;
-        let sim_options = sim_options_generic;
+        let elf_path;
+        let lib_elf;
+        elf_path = device.eth_path;
+        // Edit this: replace `Boilerplate` by your plugin name
+        lib_elf = { 'Boilerplate': device.path };
 
-        if (device === "nanos") {
-            eth_path = NANOS_ETH_PATH;
-            plugin = NANOS_PLUGIN;
-            sim_options.model = "nanos";
-        } else {
-            eth_path = NANOX_ETH_PATH;
-            plugin = NANOX_PLUGIN;
-            sim_options.model = "nanox";
-        }
-
-        const sim = new Zemu(eth_path, plugin);
-
+        const sim = new Zemu(elf_path, lib_elf);
         try {
-            await sim.start(sim_options);
+            await sim.start({...sim_options_nano, model: device.name});
             const transport = await sim.getTransport();
             const eth = new Eth(transport);
             eth.setLoadConfig({
@@ -114,6 +107,7 @@ module.exports = {
     zemu,
     waitForAppScreen,
     genericTx,
+    nano_models,
     SPECULOS_ADDRESS,
     RANDOM_ADDRESS,
     txFromEtherscan,
