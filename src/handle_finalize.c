c7772d68fb0b6d539ab5ea636748f47b0664ebca
Francois Beutin <francois.beutin@ledger.fr>
2023-11-10 10:50:57 +0100

    Fix handle_finalize parameter type and use common sdk size checker

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -1,7 +1,6 @@
 #include "plugin.h"
 
-void handle_finalize(void *parameters) {
-    ethPluginFinalize_t *msg = (ethPluginFinalize_t *) parameters;
+void handle_finalize(ethPluginFinalize_t *msg) {
     context_t *context = (context_t *) msg->pluginContext;
 
     msg->uiType = ETH_UI_TYPE_GENERIC;
