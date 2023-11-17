17af03ac2ef61d5c85148b3635af53c9a7d62493
Jorge Martins <jorge.martins@ledger.fr>
2023-11-17 15:05:23 +0100

    improve the fuzzer

--- fuzzing/mocks.c
+++ fuzzing/mocks.c
@@ -1,26 +1,5 @@
 #include "plugin.h"
 
-cx_err_t cx_keccak_init_no_throw(cx_sha3_t *hash, size_t size) {
-    UNUSED(hash);
-    UNUSED(size);
-    return CX_OK;
-}
-
-cx_err_t cx_hash_no_throw(cx_hash_t *hash,
-                          uint32_t mode,
-                          const uint8_t *in,
-                          size_t len,
-                          uint8_t *out,
-                          size_t out_len) {
-    UNUSED(hash);
-    UNUSED(mode);
-    UNUSED(in);
-    UNUSED(len);
-    UNUSED(out);
-    UNUSED(out_len);
-    return CX_OK;
-}
-
 size_t strlcat(char *dst, const char *src, size_t size) {
     size_t srclen; /* Length of source string */
     size_t dstlen; /* Length of destination string */
