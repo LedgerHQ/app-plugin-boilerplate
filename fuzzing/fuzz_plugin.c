79939c3b4f74588fb1d13f5a363f4d6ebc5cda4e
Francois Beutin <francois.beutin@ledger.fr>
2025-10-01 13:32:54 +0200

    Cleanup plugin interface

--- fuzzing/fuzz_plugin.c
+++ fuzzing/fuzz_plugin.c
@@ -20,15 +20,6 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     ethQueryContractUI_t query_ui = {0};
     txContent_t content = {0};
 
-    // Fake sha3 context
-    cx_sha3_t sha3;
-
-    ethPluginSharedRO_t shared_ro;
-    shared_ro.txContent = &content;
-
-    ethPluginSharedRW_t shared_rw;
-    shared_rw.sha3 = &sha3;
-
     context_t context;
     const uint8_t address[ADDRESS_LENGTH] = {0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee,
                                              0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee,
@@ -53,8 +44,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
     init_contract.interfaceVersion = ETH_PLUGIN_INTERFACE_VERSION_LATEST;
     init_contract.selector = data;
-    init_contract.pluginSharedRO = &shared_ro;
-    init_contract.pluginSharedRW = &shared_rw;
+    init_contract.txContent = &content;
     init_contract.pluginContext = (uint8_t *) &context;
     init_contract.pluginContextLength = sizeof(context);
 
@@ -69,8 +59,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         provide_param.parameter = data + i;
         provide_param.parameterOffset = i;
         provide_param.pluginContext = (uint8_t *) &context;
-        provide_param.pluginSharedRO = &shared_ro;
-        provide_param.pluginSharedRW = &shared_rw;
+        provide_param.txContent = &content;
         handle_provide_parameter(&provide_param);
         if (provide_param.result != ETH_PLUGIN_RESULT_OK) {
             return 0;
@@ -80,8 +69,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
     finalize.pluginContext = (uint8_t *) &context;
     finalize.address = address;
-    finalize.pluginSharedRO = &shared_ro;
-    finalize.pluginSharedRW = &shared_rw;
+    finalize.txContent = &content;
     handle_finalize(&finalize);
     if (finalize.result != ETH_PLUGIN_RESULT_OK) {
         return 0;
@@ -89,8 +77,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
     if (finalize.tokenLookup1 || finalize.tokenLookup2) {
         provide_info.pluginContext = (uint8_t *) &context;
-        provide_info.pluginSharedRO = &shared_ro;
-        provide_info.pluginSharedRW = &shared_rw;
+        provide_info.txContent = &content;
         if (finalize.tokenLookup1) {
             if (size - i >= sizeof(extraInfo_t)) {
                 provide_info.item1 = &item1;
@@ -118,8 +105,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     }
 
     query_id.pluginContext = (uint8_t *) &context;
-    query_id.pluginSharedRO = &shared_ro;
-    query_id.pluginSharedRW = &shared_rw;
+    query_id.txContent = &content;
     query_id.name = name;
     query_id.nameLength = sizeof(name);
     query_id.version = version;
@@ -139,8 +125,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         query_ui.msg = msg;
         query_ui.msgLength = sizeof(msg);
         query_ui.pluginContext = (uint8_t *) &context;
-        query_ui.pluginSharedRO = &shared_ro;
-        query_ui.pluginSharedRW = &shared_rw;
+        query_ui.txContent = &content;
 
         query_ui.screenIndex = i;
         handle_query_contract_ui(&query_ui);
